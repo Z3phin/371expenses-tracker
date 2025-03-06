7afcdd21ad42326aa8cf7f89658753d40bab0d2d
Matthew Prickett <matthew.prickett@hotmail.co.uk>
2025-03-06 11:16:49 +0000

    Pass test4

--- Assignment/371expenses-framework/src/testing.cpp
+++ Assignment/371expenses-framework/src/testing.cpp
@@ -12,11 +12,14 @@
 int main() {
     Category c{"categoryIdent"};  
     c.newItem("1", "new test", 99.99, Date()).addTag("hi"); 
-    c.addItem(Item("2", "another test", 99.99, Date()));
+    c.addItem(Item("1", "another test", 99.99, Date()));
 
     Category c1{"categoryIdent"}; 
-    c1.newItem("1", "new test", 99.99, Date()).addTag("hi");
+    c1.newItem("1", "new test", 99.99, Date());
     c1.addItem(Item("2", "another test", 99.99, Date()));
 
-    std::cout << ((c == c1) ? "true" : "false") << std::endl;
+    std::cout << c.str() << "\n\n" << std::endl;
+    std::cout << c1.str() << std::endl;
+
+    
 }
